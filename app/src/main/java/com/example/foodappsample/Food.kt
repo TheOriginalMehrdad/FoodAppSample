dbce881a04f35b0d5b9670b4dd0411ab01701da2
Mehrdad S.F. <meddisommer@gmail.com>
2024-03-01 13:57:07 +0330

    Finish the ui, commit

--- app/src/main/java/com/example/foodappsample/Food.kt
+++ app/src/main/java/com/example/foodappsample/Food.kt
@@ -3,10 +3,10 @@ package com.example.foodappsample
 
 data class Food(
     val subjectData: String,
-    val cityData: String,
     val priceData: String,
     val distanceData: String,
+    val cityData: String,
     val imageUrlData: String,
-    val ratingData: Float,
-    val ratingNumberData: String,
+    val ratingNumberData: Int,
+    val ratingData: Float
 )
--- app/src/main/java/com/example/foodappsample/FoodAdapter.kt
+++ app/src/main/java/com/example/foodappsample/FoodAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.foodappsample
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
@@ -11,26 +12,28 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import jp.wasabeef.glide.transformations.RoundedCornersTransformation
 
-class FoodAdapter(private val data: List<Food>, private val context: Context) :
+class FoodAdapter(private val data: ArrayList<Food>) :
     RecyclerView.Adapter<FoodAdapter.FoodViewHolder>() {
 
-    inner class FoodViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    inner class FoodViewHolder(itemView: View, private val context: Context) :
+        RecyclerView.ViewHolder(itemView) {
 
         private val image = itemView.findViewById<ImageView>(R.id.item_img_main)
         private val subject = itemView.findViewById<TextView>(R.id.item_txt_subject)
-        private val city = itemView.findViewById<TextView>(R.id.item_txt_subject)
-        private val price = itemView.findViewById<TextView>(R.id.item_txt_city)
+        private val city = itemView.findViewById<TextView>(R.id.item_txt_city)
+        private val price = itemView.findViewById<TextView>(R.id.item_txt_price)
         private val distance = itemView.findViewById<TextView>(R.id.item_txt_distance)
         private val myRatingBar = itemView.findViewById<RatingBar>(R.id.item_rating_bar)
         private val myRatingNumber = itemView.findViewById<TextView>(R.id.item_rating_number)
 
+        @SuppressLint("SetTextI18n")
         fun bindData(position: Int) {
             subject.text = data[position].subjectData
             city.text = data[position].cityData
-            price.text = data[position].priceData
-            distance.text = data[position].distanceData
+            price.text = "$${data[position].priceData} vip"
+            distance.text = "${data[position].distanceData} miles from you"
             myRatingBar.rating = data[position].ratingData
-            myRatingNumber.text = data[position].ratingNumberData
+            myRatingNumber.text = "(${data[position].ratingNumberData}) Ratings"
 
             Glide.with(context).load(data[position].imageUrlData).transform(
                 RoundedCornersTransformation(18, 6)
@@ -43,7 +46,7 @@ class FoodAdapter(private val data: List<Food>, private val context: Context) :
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FoodViewHolder {
 
         val view = LayoutInflater.from(parent.context).inflate(R.layout.food_item, parent, false)
-        return FoodViewHolder(view)
+        return FoodViewHolder(view, parent.context)
     }
 
     override fun getItemCount(): Int {
